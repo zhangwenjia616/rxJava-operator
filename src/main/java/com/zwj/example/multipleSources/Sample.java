c3e0f30120639fb74617a33cbf9d8af253ff8187
zhangwenjia <1002349243@qq.com>
2016-12-21 13:27:34 +0800

    dd

--- src/main/java/com/zwj/example/multipleSources/Sample.java
+++ src/main/java/com/zwj/example/multipleSources/Sample.java
@@ -10,16 +10,23 @@ public class Sample {
     Sources sources = new Sources();
 
     // Create our sequence for querying best available data
-    Observable<Data> source = Observable.concat(
-        sources.memory(), // 先从内存中取 看是否是符合first中的条件的
-        sources.disk(), // 再从磁盘里取
-        sources.network()) // 再从网络上获取
+    Observable<Data> source = Observable
+        .concat(sources.memory(), // 先从内存中取 看是否是符合first中的条件的
+            sources.disk(), // 再从磁盘里取
+            sources.network()) // 再从网络上获取
+        // .first();
         .first(data -> data != null && data.isUpToDate());
 
     // "Request" latest data once a second
-    Observable.interval(1, TimeUnit.SECONDS).flatMap(__ -> source) // 跟输入的秒数无关 所以是__ 返回的是source
-                                                                   // Observable 主要是返回source
-        .subscribe(data -> System.out.println("##Received## " + data.value)); // 在这里订阅
+    // 跟输入的秒数无关 所以是__ 返回的是source
+    // Observable 主要是返回source
+    Observable.interval(1, TimeUnit.SECONDS).flatMap(__ -> source).subscribe(data -> {
+      if (data != null) {
+        System.out.println("##Received## " + data.value);
+      } else {
+        System.out.println("##Received##" + " but data is null");
+      }
+    }); // 在这里订阅
 
     // Occasionally clear memory (as if app restarted) so that we must go to disk
     // 每隔三秒钟 清理内存
